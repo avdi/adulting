82a9d619a8f0f71550a75e51865d4a04fd286446
Avdi Grimm <avdi@avdi.codes>
2023-12-02 04:15:27 +0000

    Noodling on yielding

--- lib/google_sheets_gateway.rb
+++ lib/google_sheets_gateway.rb
@@ -1,21 +1,12 @@
 module GoogleSheetsGateway
   class AchieveCurrentCredentials
-    def initialize
-      @stage = :initial
-    end
-
-    def next(provide: {})
-      credentials = {
-        client_id: @client_id ||= provide[:client_id],
-        client_secret: @client_secret ||= provide[:client_secret]
-      }
-      case @stage
-      in :initial then
-        if credentials.all? { _2 } then @stage = :get_token_store end
-        {status: :blocked, need: credentials.reject { _2 }, stage: @stage}
-      in :get_token_store
-        {status: :ready, need: {}, stage: @stage}
+    def proceed
+      needs = {client_id: nil, client_secret: nil}
+      until needs.all? { _2 }
+        yield(intent: :get_credentials, status: :blocked, need: needs.filter { _2.nil? }.keys) => {provide:}
+        needs.merge! provide
       end
+      yield(intent: :get_token_store, status: :ready, need: {}) => request until Hash(request)[:to] == :get_token_store
     end
   end
 end
--- spec/google_sheets_gateway_spec.rb
+++ spec/google_sheets_gateway_spec.rb
@@ -8,18 +8,20 @@ describe GoogleSheetsGateway do
 
   describe GoogleSheetsGateway::AchieveCurrentCredentials do
     it "can be instantiated" do
-      it = GoogleSheetsGateway::AchieveCurrentCredentials.new
-      it.next => {status:, need:, stage:}
-      expect(stage).to eq(:initial)
+      it = GoogleSheetsGateway::AchieveCurrentCredentials.new.enum_for(:proceed)
+      it.next => {status:, need:, intent:}
+      expect(intent).to eq(:get_credentials)
       expect(status).to eq(:blocked)
       expect(need).to include(:client_id, :client_secret)
-      it.next(provide: {client_id: "cid1234"}) => {status:, need:}
+      it.feed provide: {client_id: "cid1234"}
+      it.next => {status:, need:}
       expect(need).not_to include(:client_id)
-      it.next(provide: {client_secret: "sec5678"}) => {status:, need:}
+      it.feed provide: {client_secret: "sec5678"}
+      it.next => {status:, need:}
       expect(need).not_to include(:client_secret)
-      it.next => {status:, stage:, need:}
+      it.next => {status:, intent:, need:}
       expect(status).to eq(:ready)
-      expect(stage).to eq(:get_token_store)
+      expect(intent).to eq(:get_token_store)
     end
   end
 end
